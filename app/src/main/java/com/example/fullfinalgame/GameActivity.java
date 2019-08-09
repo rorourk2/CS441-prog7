fa609a3f9570d7ffac1866baf28f274de5c8a434
Ryan O'Rourke <rorourk2@binghamton.edu>
2019-08-08 23:14:44 -0400

    game

--- app/src/main/java/com/example/fullfinalgame/GameActivity.java
+++ app/src/main/java/com/example/fullfinalgame/GameActivity.java
@@ -1,13 +1,22 @@
 package com.example.fullfinalgame;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        //setContentView(R.layout.activity_game);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
     }
 }
